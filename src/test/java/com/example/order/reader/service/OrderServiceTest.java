0b05e93eab87b0fcfe6d4fa7324f730e77fe0e02
franco-ednaldo <ednaldofranco.dev@gmail.com>
2023-08-07 11:32:15 -0300

    finalizando test unitario da classe de servico

--- src/test/java/com/example/order/reader/service/OrderServiceTest.java
+++ src/test/java/com/example/order/reader/service/OrderServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,7 +15,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static com.example.order.reader.service.mock.CustomerMock.customerMock;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
@@ -40,18 +39,36 @@ class OrderServiceTest {
     @DisplayName("Método que recebe um arquivo .txt e faz o parser com sucesso")
     void givenAValidFiles_whenCallProcess_thenShouldParserFiles() throws IOException {
         final var nameFile = "order.txt";
-        MultipartFile file = new MockMultipartFile("order.txt", "order.txt", "", "test-content".getBytes());
+        MultipartFile file = new MockMultipartFile(nameFile, nameFile, "", "test-content".getBytes());
         when(processFileMock.accept(any())).thenReturn(true);
         when(processFileMock.process(any())).thenReturn(customerMock());
 
         final var response = orderService.process(List.of(file));
 
         assertEquals(1, response.size());
-        Mockito.verify(processFileMock, times(1)).accept(any());
-        Mockito.verify(processFileMock, times(1)).process(any());
+        verify(processFileMock, times(1)).accept(any());
+        verify(processFileMock, times(1)).process(any());
         verifyNoMoreInteractions(processFileMock);
-
     }
 
+    @Test
+    @DisplayName("Método que recebe um arquivo .txt e retorna exeptiion caso o tipo de uma arquivo for inválido")
+    void givenAnInValidFiles_whenCallProcess_thenShouldException() throws IOException {
+        final var nameFile = "order.pdf";
+        final var exceptedMessage = "Type of process not found.";
+
+        MultipartFile file = new MockMultipartFile(nameFile, nameFile, "", "test-content".getBytes());
+        when(processFileMock.accept(any())).thenReturn(false);
+
+        final var exception = assertThrows(Exception.class, () -> {
+            orderService.process(List.of(file));
+        });
+
+
+        assertTrue(exception.getMessage().contains(exceptedMessage));
+        verify(processFileMock, times(1)).accept(any());
+        verify(processFileMock, never()).process(any());
+        verifyNoMoreInteractions(processFileMock);
+    }
 
 }
